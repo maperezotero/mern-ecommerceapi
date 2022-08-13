6b80535355eea1624ea285dec515553ac611d0be
maperezotero <maperezotero@gmail.com>
2022-08-13 18:57:21 +0200

    MongoDB connection

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hi there!');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+	.then(()=> console.log('DB Connection Successful!'))
+	.catch((err) => {
+		console.log(err);
+	});
+
+app.listen( process.env.PORT || 3001, () => {
+	console.log('Server running on http://localhost:3001');
+});
