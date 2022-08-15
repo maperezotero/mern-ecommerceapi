7bd0927721459e1d3030ffc294d3e95e908a0c75
maperezotero <maperezotero@gmail.com>
2022-08-15 14:06:22 +0200

    Testing with React client OK

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
 const cartRoute = require('./routes/cart');
 const orderRoute = require('./routes/order');
+const stripeRoute = require('./routes/stripe');
+const cors = require('cors');
 
-dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL)
 	.then(()=> console.log('DB Connection Successful!'))
 	.catch((err) => {
 		console.log(err);
 });
+
+app.use(cors());
 
 app.use(express.json());
 
@@ -23,6 +27,7 @@ app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
 app.use('/api/carts', cartRoute);
 app.use('/api/orders', orderRoute);
+app.use('/api/checkout', stripeRoute);
 
 app.listen( process.env.PORT || 3001, () => {
 	console.log('Server running on http://localhost:3001');
